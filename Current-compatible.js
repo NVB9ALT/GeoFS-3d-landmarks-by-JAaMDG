b743b8ea187c6f26e19a288c04440fe331241204
NVB9ALT <102188956+NVB9ALT@users.noreply.github.com>
2022-09-30 21:27:58 -0700

    Create Current-compatible.js

---
+++ Current-compatible.js
@@ -0,0 +1,129 @@
+console.log("GeoFS 3d Landmark %C2%A9%EF%B8%8F2020-2021 ExtendDevelopment Inc, all rights reserved. Modified for compatibility with GeoFS versions 3.0 and higher by NVB9.");
+geofs.objects.objectList = []
+geofs.objects.objectList[0] = {
+   "location": [42.75, 41.25, 0],
+   "url": "https://geo-fs.com/models/objects/carrier/carrier.gltf",
+   "htr": [180, 0, 0],
+   "scale": 1,
+   "options": { "shadows": 1 }, "type": 100
+}
+geofs.objects.objectList[1] = {
+   "location": [40.7668364267734, -74.0029338852795, 0],
+   "url": "https://geo-fs.com/models/objects/carrier/carrier.gltf",
+   "htr": [110, 0, 0],
+   "scale": 1,
+   "options": { "shadows": 1 }, "type": 100
+}
+geofs.objects.objectList[2] = {
+   "location": [40.713003, -74.013184, 20],
+   "url": "https://129066664-566439557718181686.preview.editmysite.com/uploads/1/2/9/0/129066664/onewtc.gltf",
+   "htr": [0, 0, 0],
+   "scale": 1,
+   "options": { "shadows": 1 }, "type": 100
+}
+geofs.objects.objectList[3] = {
+   "location": [40.642468, -73.781306, 0],
+   "url": "https://geo-fs.com/backend/aircraft/repository/3D%20buildings_267286_3670/jfkatc.glb",
+   "htr": [0, 0, 0],
+   "scale": 1,
+   "options": { "shadows": 1 }, "type": 100
+}
+geofs.objects.objectList[4] = {
+   "location": [33.944006649127665, -118.40384478693352, 0],
+   "url": "https://geo-fs.com/backend/aircraft/repository/3D%20buildings_267286_3670/genericatc.glb",
+   "htr": [0, 0, 0],
+   "scale": 1,
+   "options": { "shadows": 1 }, "type": 100
+}
+geofs.objects.objectList[5] = {
+   "location": [51.47178486314496, -0.4654623912115562, 0],
+   "url": "https://geo-fs.com/backend/aircraft/repository/3D%20buildings_267286_3670/genericatc.glb",
+   "htr": [0, 0, 0],
+   "scale": 1,
+   "options": { "shadows": 1 }, "type": 100
+}
+geofs.objects.objectList[6] = {
+   "location": [49.003465520509174, 2.573483109426901, 0],
+   "url": "https://geo-fs.com/backend/aircraft/repository/3D%20buildings_267286_3670/genericatc.glb",
+   "htr": [0, 0, 0],
+   "scale": 1,
+   "options": { "shadows": 1 }, "type": 100
+}
+geofs.objects.objectList[7] = {
+   "location": [49.003465520509174, 2.573483109426901, 0],
+   "url": "https://geo-fs.com/backend/aircraft/repository/3D%20buildings_267286_3670/genericatc.glb",
+   "htr": [0, 0, 0],
+   "scale": 1,
+   "options": { "shadows": 1 }, "type": 100
+}
+geofs.objects.objectList[8] = {
+   "location": [37.615913114472754, -122.3838715058867, 0],
+   "url": "https://geo-fs.com/backend/aircraft/repository/3D%20buildings_267286_3670/genericatc.glb",
+   "htr": [0, 0, 0],
+   "scale": 1,
+   "options": { "shadows": 1 }, "type": 100
+}
+geofs.objects.objectList[9] = {
+   "location": [25.797520104826276, -80.28080030099528, 0],
+   "url": "https://geo-fs.com/backend/aircraft/repository/3D%20buildings_267286_3670/genericatc.glb",
+   "htr": [0, 0, 0],
+   "scale": 1,
+   "options": { "shadows": 1 }, "type": 100
+}
+geofs.objects.objectList[10] = {
+   "location": [38.62467160719759, -90.1849294458611, 0],
+   "url": "https://geo-fs.com/backend/aircraft/repository/3D%20buildings_267286_3670/gateway.glb",
+   "htr": [0, 0, 0],
+   "scale": 1,
+   "options": { "shadows": 1 }, "type": 100
+}
+geofs.objects.objectList[11] = {
+   "location": [-33.85709212131164, 151.2151553844974, 0],
+   "url": "https://geo-fs.com/backend/aircraft/repository/3D%20buildings_267286_3670/operahouse.glb",
+   "htr": [0, 0, 0],
+   "scale": 1,
+   "options": { "shadows": 1 }, "type": 100
+}
+geofs.objects.objectList[12] = {
+   "location": [51.50439997167663, -0.0865319044008741, 60],
+   "url": "https://geo-fs.com/backend/aircraft/repository/3D%20buildings_267286_3670/shard_2.glb",
+   "htr": [0, 0, 0],
+   "scale": 1,
+   "options": { "shadows": 1 }, "type": 100
+}
+geofs.objects.objectList[13] = {
+   "location": [41.87888906449908, -87.63587381204945, 100],
+   "url": "https://geo-fs.com/backend/aircraft/repository/3D%20buildings_267286_3670/sears_towers.glb",
+   "htr": [0, 0, 0],
+   "scale": 1,
+   "options": { "shadows": 1 }, "type": 100
+}
+geofs.objects.objectList[14] = {
+   "location": [47.62054867355818, -122.34929125332603, 0],
+   "url": "https://geo-fs.com/backend/aircraft/repository/3D%20buildings_267286_3670/spaceneedle.glb",
+   "htr": [0, 0, 0],
+   "scale": 1,
+   "options": { "shadows": 1 }, "type": 100
+}
+geofs.objects.objectList[15] = {
+   "location": [51.514478444303336, -0.0803259937534029, 60],
+   "url": "https://geo-fs.com/backend/aircraft/repository/3D%20buildings_267286_3670/gherkin_2.glb",
+   "htr": [0, 0, 0],
+   "scale": 1,
+   "options": { "shadows": 1 }, "type": 100
+}
+geofs.objects.objectList[16] = {
+   "location": [43.72301853292317, 10.39663055392561, 30],
+   "url": "https://geo-fs.com/backend/aircraft/repository/3D%20buildings_267286_3670/leaning_tower_of_pisa_2.glb",
+   "htr": [0, 0, 0],
+   "scale": 1,
+   "options": { "shadows": 1 }, "type": 100
+}
+geofs.objects.objectList[17] = {
+   "location": [51.50072163386705, -0.12460950809854282, 50],
+   "url": "https://129066664-566439557718181686.preview.editmysite.com/uploads/1/2/9/0/129066664/big_ben-_for_project_proper_h.glb",
+   "htr": [0, 0, 0],
+   "scale": 1,
+   "options": { "shadows": 1 }, "type": 100
+}
+   geofs.objects.loadModels();
